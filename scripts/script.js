84a95004aa7adc40e2a309be478491f30a7ae4ac
Lisaroelvink <lisa.roelvink2@hva.nl>
2021-11-11 15:55:12 +0100

    links en hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+var deButton = document.querySelector("nav button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+    console.log("hoi")
+        // https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+    var deNav = event.target.closest("nav");
+    console.log(event.target);
+    deNav.classList.toggle("toonMenu");
+}
